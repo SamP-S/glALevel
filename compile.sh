412558466a68cab0455cda0dc36bef3b674438aa
SamP-S <sam.ps1@outlook.com>
2024-01-09 15:49:41 +0000

    v0.2

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-g++ src/OpenGL_0.1.cpp -o bin/exec -Iexternal/GLFW/include -Lexternal/GLFW/lib -lglfw
+g++ src/OpenGL_0.1.cpp -o bin/v0.1 -Iexternal/GLFW/include -Lexternal/GLFW/lib -lglfw
+g++ src/OpenGL_0.2.cpp -o bin/v0.2 -Iexternal/GLFW/include -Iexternal/GLEW/include -Lexternal/GLFW/lib -Lexternal/GLEW/lib -lGLEW -lGL -lglfw 
